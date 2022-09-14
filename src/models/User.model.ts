b540ed78d377417afe4f4df621dc292020038653
Leonardo Lima <leoblima00@gmail.com>
2022-09-14 18:27:07 -0300

    Feat: adds new functionality to user model: search by username

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -8,6 +8,16 @@ class UserModel {
     this.connection = connection;
   }
 
+  public async getByUsername(username: string): Promise<User[]> {
+    const result = await this.connection.execute(
+      'SELECT * Trybesmith.Users WHERE username = ?;',
+      [username],
+    );
+
+    const [data] = result;
+    return data as User[];
+  }
+
   public async create(user: User): Promise<User> {
     const { username, classe, level, password } = user;
     const result = await this.connection.execute<ResultSetHeader>(
