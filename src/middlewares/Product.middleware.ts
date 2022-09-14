9a254ecd029b4405ae3702b552b00495c6e0555e
Leonardo Lima <leoblima00@gmail.com>
2022-09-14 20:12:32 -0300

    Feat: adds new verifications to post /product endpoint

--- src/middlewares/Product.middleware.ts
+++ src/middlewares/Product.middleware.ts
@@ -3,6 +3,30 @@ import Product from '../interfaces/Product.interface';
 
 const properties = ['name', 'amount'];
 
+const LENGTH_MESSAGE = 'length must be at least 3 characters long';
+
+function validateLength(product: Product): [boolean, string | null] {
+  const entries = Object.entries(product);
+  for (let i = 0; i < entries.length; i += 1) {
+    const [property, value] = entries[i];
+    if (value.length < 3) {
+      return [false, property];
+    }
+  }
+  return [true, null];
+}
+
+function validateTypes(product: Product): [boolean, string | null] {
+  const entries = Object.entries(product);
+  for (let i = 0; i < entries.length; i += 1) {
+    const [property, value] = entries[i];
+    if (typeof value !== 'string') {
+      return [false, property];
+    }
+  }
+  return [true, null];
+}
+
 function validateProperties(product: Product): [boolean, string | null] {
   for (let i = 0; i < properties.length; i += 1) {
     if (!Object.prototype.hasOwnProperty.call(product, properties[i])) {
@@ -29,17 +53,25 @@ function validationProduct(req: Request, res: Response, next: NextFunction) {
   let [valid, property] = validateProperties(product);
 
   if (!valid) {
-    return res.status(403).send(
-      `O campo ${property} é obrigatório.`,
-    );
+    return res.status(400).json({ message: `"${property}" is required` });
   }
 
   [valid, property] = validateValues(product);
 
   if (!valid) {
-    return res.status(403).send(
-      `O campo ${property} não pode ser nulo ou vazio.`,
-    );
+    return res.status(400).json({ message: `"${property}" is required` });
+  }
+  
+  [valid, property] = validateTypes(product);
+
+  if (!valid) {
+    return res.status(422).json({ message: `"${property}" must be a string` });
+  }
+
+  [valid, property] = validateLength(product);
+
+  if (!valid) {
+    return res.status(422).json({ message: `"${property}" ${LENGTH_MESSAGE}` });
   }
 
   next();
